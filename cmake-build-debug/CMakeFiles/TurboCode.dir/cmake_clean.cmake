0a76aa53d73ba3cebc03e571ca3148e2dcdbb6a5
Hugo Fernández Solís <hfernandezs@sobremesa-casa.localdomain>
2022-03-16 14:45:12 +0000

    Inicialización del repositorio

---
+++ cmake-build-debug/CMakeFiles/TurboCode.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TurboCode.dir/src/main.cpp.o"
+  "TurboCode"
+  "TurboCode.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TurboCode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
